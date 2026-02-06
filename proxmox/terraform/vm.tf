48eee4420e1e56ee27cb55f9e1ae9b11f3c34119
Greg Brown <gregjbrown@hotmail.com>
2026-02-06 21:21:23 +0000

    Add Proxmox VM configuration in vm.tf

--- proxmox/terraform/vm.tf
+++ proxmox/terraform/vm.tf
@@ -1,1 +1,34 @@
+resource "proxmox_virtual_environment_vm" "example" {
+  name      = "tf-test-01"
+  node_name = "proxmox"
+  pool_id   = "terraform-vms"
 
+  clone {
+    vm_id = 9000 # template VMID
+  }
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_account {
+      username = "debian"
+      ssh_keys = [file("~/.ssh/id_rsa.pub")]
+    }
+  }
+}
